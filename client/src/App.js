464132386eefe49c1cb66d8ab362b6215cc53380
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-12-15 19:31:22 -0800

    finished MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import BubblePage from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -8,9 +10,16 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
+        <Link to="/">Login</Link>
+        <Link to="/protected">Bubbles</Link>
+
+        <Switch>
+          <PrivateRoute exact path="/protected" component={BubblePage} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
         {/* 
-          Build a PrivateRoute component that will 
+          Build a PrivateRoute component that wiZll 
           display BubblePage when you're authenticated 
         */}
       </div>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Login = () => {
+const Login = props => {
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  useEffect(() => {
+    localStorage.removeItem("token");
+  }, []);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios.post("http://localhost:5000/api/login", login).then(res => {
+      localStorage.setItem("token", res.data.payload);
+      props.history.push("/protected");
+    });
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          onChange={handleChange}
+          placeholder="Username"
+          value={login.username}
+        />
+        <input
+          type="text"
+          name="password"
+          onChange={handleChange}
+          placeholder="Password"
+          value={login.password}
+        />
+        <button type="submit">Log in</button>
+        <button
+          onClick={e => {
+            e.preventDefault();
+            localStorage.removeItem("token");
+          }}
+        >
+          Log out
+        </button>
+      </form>
     </>
   );
 };
